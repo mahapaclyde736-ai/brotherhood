14a3548574f6ce1ba1a1757fdf7c10db86989068
Emmanuel Chigwende <emmanuelchigwende2@gmail.com>
2026-06-04 19:49:34 +0200

    important please read dude

--- backend/src/Database/Connection.js
+++ backend/src/Database/Connection.js
@@ -2,6 +2,7 @@ import mongoose, { mongo } from "mongoose";
 
 function DatabaseCon(){
     // connection string goes here
+    // dude make sure you have mongo db installed or it wont work this runs on your pc no wifi required
     mongoose.connect("mongodb://127.0.0.1:27017/Clockin")
     console.log("database connected successfully")
 }
